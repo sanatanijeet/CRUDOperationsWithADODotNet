55afb4b6cd3d5f27ad32bc31eefda50d87fe608a
jeetyadu <visyadav9005@gmail.com>
2023-11-17 17:19:19 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUDOperationsWithADODotNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
